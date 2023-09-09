8f49dc1dcc66b96f81a329d600509d402ecca1b6
Leno Tavares Cabral <leno.tavares.cabral@gmail.com>
2023-09-09 16:36:10 -0300

    end chap 2

--- src/main/java/com/sec/config/ProjectConfig.java
+++ src/main/java/com/sec/config/ProjectConfig.java
@@ -1,7 +1,6 @@
 package com.sec.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import com.sec.security.CustomAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,9 +12,14 @@ import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
-@Configuration
 public class ProjectConfig {
-    @Bean
+
+    private final CustomAuthenticationProvider authenticationProvider;
+
+    public ProjectConfig(CustomAuthenticationProvider authenticationProvider) {
+        this.authenticationProvider = authenticationProvider;
+    }
+
     public UserDetailsService userDetailsService() {
         UserDetails user = User
                 .withUsername("john")
@@ -25,17 +29,35 @@ public class ProjectConfig {
         return new InMemoryUserDetailsManager(user);
     }
 
-    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.httpBasic(withDefaults());
+
+        http.authenticationProvider(authenticationProvider);
+
         http.authorizeHttpRequests(
                 c -> c.anyRequest().authenticated()
         );
         return http.build();
     }
 
-    @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+    }
+
+
+    // Another Away to configure UserDetailsService
+    public SecurityFilterChain filterChainConfig2(HttpSecurity http) throws Exception {
+        http.httpBasic(withDefaults());
+        http.authorizeHttpRequests(
+                c -> c.anyRequest().authenticated()
+        );
+
+        var user = User.withUsername("john").password("12345").authorities("read").build();
+
+        var userDetailsService = new InMemoryUserDetailsManager(user);
+
+        http.userDetailsService(userDetailsService);
+
+        return http.build();
     }
 }
---
+++ src/main/java/com/sec/config/WebAuthorizationConfig.java
@@ -0,0 +1,23 @@
+package com.sec.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class WebAuthorizationConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.httpBasic(withDefaults());
+
+        http.authorizeHttpRequests(
+                c -> c.anyRequest().authenticated()
+        );
+
+        return http.build();
+    }
+}
